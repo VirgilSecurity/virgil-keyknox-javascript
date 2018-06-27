47173b29e02a3dae4d3061e26aade460b639b42c
Serhii Nanovskyi <s.nanovskyi@gmail.com>
2018-06-27 13:19:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Keyknox JavaScript SDK
---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const typescript = require('rollup-plugin-typescript2');
+
+const packageJson = require('./package.json');
+
+const env = process.env.NODE_ENV;
+
+module.exports = {
+  input: path.join(__dirname, 'src', 'index.ts'),
+  output: {
+    file: `${packageJson.name}.${env}.js`,
+    format: env,
+    dir: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    typescript({ useTsconfigDeclarationDir: true }),
+  ],
+};
