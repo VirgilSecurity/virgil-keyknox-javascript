9613f5fae7189a8d5920d04735f10bfdb5cb8c51
Serhii Nanovskyi <s.nanovskyi@gmail.com>
2018-07-30 16:36:17 +0300

    Update build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,37 @@
 const path = require('path');
+const commonjs = require('rollup-plugin-commonjs');
+const inject = require('rollup-plugin-inject');
+const nodeGlobals = require('rollup-plugin-node-globals');
+const resolve = require('rollup-plugin-node-resolve');
 const typescript = require('rollup-plugin-typescript2');
 
 const packageJson = require('./package.json');
 
-const env = process.env.NODE_ENV;
+const env = process.env.ENV;
+const format = process.env.FORMAT;
+
+const browser = 'browser';
+const node = 'node';
+const cjs = 'cjs';
+const es = 'es';
+const umd = 'umd';
+const environment = {
+  [browser]: {
+    [cjs]: cjs,
+    [es]: es,
+    [umd]: umd,
+  },
+  [node]: {
+    [cjs]: cjs,
+    [es]: es,
+  },
+};
+
+if (!environment[env]) {
+  throw new TypeError(`'${env}' is not a valid environment`);
+} else if (!environment[env][format]) {
+  throw new TypeError(`'${format}' is not a valid module format for '${env}' environment`);
+}
 
 module.exports = {
   input: path.join(__dirname, 'src', 'index.ts'),
@@ -12,14 +40,27 @@ module.exports = {
     Object.keys(packageJson.peerDependencies),
   ),
   output: {
-    file: `${packageJson.name}.${env}.js`,
-    format: env,
+    format,
+    file: `${packageJson.name}.${env}.${format}.js`,
     dir: path.join(__dirname, 'dist'),
+    name: 'Keyknox',
+    globals: {
+      ['virgil-crypto']: 'VirgilCrypto',
+      ['virgil-sdk']: 'Virgil',
+    },
   },
   plugins: [
+    format === umd && resolve({ browser: true }),
     typescript({
       exclude: ['**/*.test.ts', '**/*.spec.ts', '**/__mocks__/*.ts'],
       useTsconfigDeclarationDir: true,
     }),
+    env === browser && nodeGlobals(),
+    env === browser && inject({
+      modules: {
+        Buffer: ['buffer-es6', 'Buffer'],
+      },
+    }),
+    format === umd && commonjs(),
   ],
 };
