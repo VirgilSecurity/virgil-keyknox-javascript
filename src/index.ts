571ea37e094e59672bf9ccfa20434e9fc71e12e1
Serhii Nanovskyi <s.nanovskyi@gmail.com>
2018-08-01 17:38:54 +0300

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 export { default as IKeyknoxClient } from './clients/IKeyknoxClient';
-export { default as KeyknoxClient } from './clients/KeyknoxClient';
-
 export { default as IKeyknoxCrypto } from './cryptos/IKeyknoxCrypto';
-export { default as KeyknoxCrypto } from './cryptos/KeyknoxCrypto';
 
 export * from './errors';
 
 export { default as CloudKeyStorage } from './CloudKeyStorage';
 export { default as KeyknoxManager } from './KeyknoxManager';
 export { default as SyncKeyStorage } from './SyncKeyStorage';
+
+const buffer = Buffer;
+export { buffer as Buffer };
