d5fd9a54bfbaf9b161911d978d1bf691f6b82daf
Alexey Smirnov <alexey@smirnov.works>
2019-02-21 15:31:37 +0200

    update tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ const { join } = require('path');
 
 module.exports = {
   preset: 'ts-jest',
-  setupTestFrameworkScriptFile: join(__dirname, 'jest.setup.js'),
+  setupFilesAfterEnv: [join(__dirname, 'jest.setup.js')],
   testEnvironment: 'node',
 };
--- src/__tests__/CloudKeyStorage.test.ts
+++ src/__tests__/CloudKeyStorage.test.ts
@@ -285,7 +285,7 @@ describe('CloudKeyStorage', () => {
     );
     cloudKeyStorage = new CloudKeyStorage(keyknoxManager);
     await keyknoxManager.pushValue(Buffer.from(uuid()));
-    await cloudKeyStorage.deleteAllEntries();
+    CloudKeyStorage.resetAllEntries(accessTokenProvider);
     await cloudKeyStorage.retrieveCloudEntries();
     const entries = cloudKeyStorage.retrieveAllEntries();
     expect(entries).toHaveLength(0);
