20da5e0c91d75f1c5b68e6724f9c98c6ebf4b04e
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-08-09 16:48:31 +0300

    Update README.md and example

---
+++ example/README.md
@@ -0,0 +1,19 @@
+# Virgil Keyknox JavaScript SDK Usage Example
+Client-server application that shows basics of `@virgilsecurity/keyknox`.
+
+## Preparation
+There are few mandatory steps that you need to complete:
+- Create Application in [Virgil Dashboard][_dashboard]
+- Create API Key in [Virgil Dashboard][_dashboard]
+- Create a new file named `.env` with the contents of `.env.example`
+  ```sh
+  cp .env.example .env
+  ```
+- Open the `.env` file in a text editor and replace the values starting with `[VIRGIL_...` with the corresponding values from [Virgil Dashboard][_dashboard]
+
+## Usage
+You need to complete sequence from two steps:
+- Launch `server.js`
+- Launch `client.js`
+
+[_dashboard]: https://dashboard.virgilsecurity.com
---
+++ example/client.js
@@ -0,0 +1,119 @@
+const { get } = require('http');
+const { stringify } = require('querystring');
+const dotenv = require('dotenv');
+const uuid = require('uuid/v4');
+const { initCrypto, VirgilCrypto } = require('virgil-crypto');
+const { KeyEntryStorage, CachingJwtProvider } = require('virgil-sdk');
+
+const { SyncKeyStorage } = require('../dist/keyknox.cjs');
+
+dotenv.config();
+
+const config = {
+  endpoint: `http://localhost:${process.env.PORT}`,
+  identity: uuid(),
+};
+
+function renewJwtFn() {
+  return new Promise(resolve => {
+    get(`${config.endpoint}/?${stringify({ identity: config.identity })}`, response => {
+      let rawData = '';
+      response.on('data', chunk => {
+        rawData += chunk;
+      });
+      response.on('end', () => {
+        const data = JSON.parse(rawData);
+        resolve(data.jwt);
+      });
+    });
+  });
+}
+
+initCrypto().then(() => {
+  const virgilCrypto = new VirgilCrypto();
+  const accessTokenProvider = new CachingJwtProvider(renewJwtFn);
+  const keyEntryStorage = new KeyEntryStorage();
+  const keyPair = virgilCrypto.generateKeys();
+  const syncKeyStorage = SyncKeyStorage.create({
+    accessTokenProvider,
+    keyEntryStorage,
+    identity: config.identity,
+    privateKey: keyPair.privateKey,
+    publicKeys: keyPair.publicKey,
+  });
+
+  console.log('syncing...');
+  syncKeyStorage
+    .sync()
+    .then(() => {
+      console.log('sync complete');
+      console.log('retrieving all entries...');
+      return syncKeyStorage.retrieveAllEntries();
+    })
+    .then(allEntries => {
+      console.log('all entries:', allEntries);
+      console.log('storing new entry...');
+      return syncKeyStorage.storeEntry('entry', Buffer.from('data'));
+    })
+    .then(storedEntry => {
+      console.log('stored entry:', storedEntry);
+      console.log('storing new entries...');
+      return syncKeyStorage.storeEntries([
+        { name: 'entry1', data: Buffer.from('data1') },
+        { name: 'entry2', data: Buffer.from('data2') },
+        { name: 'entry3', data: Buffer.from('data3') },
+      ]);
+    })
+    .then(storedEntries => {
+      console.log('stored entries:', storedEntries);
+      console.log('updating entry1...');
+      return syncKeyStorage.updateEntry('entry1', Buffer.from('data1Updated'));
+    })
+    .then(() => {
+      console.log('entry1 updated...');
+      console.log('retrieving entry1...');
+      return syncKeyStorage.retrieveEntry('entry1');
+    })
+    .then(entry1 => {
+      console.log('entry1:', entry1);
+      console.log('retrieving all entries...');
+      return syncKeyStorage.retrieveAllEntries();
+    })
+    .then(allEntries => {
+      console.log('all entries:', allEntries);
+      console.log('deleting entry1...');
+      return syncKeyStorage.deleteEntry('entry1');
+    })
+    .then(() => {
+      console.log('entry1 deleted');
+      console.log('retrieving all entries...');
+      return syncKeyStorage.retrieveAllEntries();
+    })
+    .then(allEntries => {
+      console.log('all entries:', allEntries);
+      console.log('deleting entry2 and entry3...');
+      return syncKeyStorage.deleteEntries(['entry2', 'entry3']);
+    })
+    .then(() => {
+      console.log('entry2 and entry3 deleted');
+      console.log('retrieving all entries...');
+      return syncKeyStorage.retrieveAllEntries();
+    })
+    .then(allEntries => {
+      console.log('all entries:', allEntries);
+      console.log('updating recipients...');
+      const newKeyPair = virgilCrypto.generateKeys();
+      return syncKeyStorage.updateRecipients({
+        newPrivateKey: newKeyPair.privateKey,
+        newPublicKeys: newKeyPair.publicKey,
+      });
+    })
+    .then(() => {
+      console.log('recipients updated');
+      console.log('retrieving all entries...');
+      return syncKeyStorage.retrieveAllEntries();
+    })
+    .then(allEntries => {
+      console.log('all entries:', allEntries);
+    });
+});
