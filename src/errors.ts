9656eea2165d284f3450473b5ed7c427eed42caa
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-23 17:31:00 +0300

    Fix custom error classes for react-native

--- src/errors.ts
+++ src/errors.ts
@@ -6,75 +6,83 @@ export class KeyknoxError extends Error {
   }
 }
 
-export class KeyknoxClientError extends KeyknoxError {
+export class KeyknoxClientError extends Error {
   status?: number;
   code?: number;
 
   constructor(message: string, status?: number, code?: number) {
     super(message);
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'KeyknoxClientError';
     this.status = status;
     this.code = code;
   }
 }
 
-export class CloudKeyStorageOutOfSyncError extends KeyknoxError {
+export class CloudKeyStorageOutOfSyncError extends Error {
   constructor() {
     super('CloudKeyStorage is out of sync');
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'CloudKeyStorageOutOfSyncError';
   }
 }
 
-export class CloudEntryExistsError extends KeyknoxError {
+export class CloudEntryExistsError extends Error {
   cloudEntryName: string;
 
   constructor(cloudEntryName: string) {
     super(`Cloud entry '${cloudEntryName}' already exists`);
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'CloudEntryExistsError';
     this.cloudEntryName = cloudEntryName;
   }
 }
 
-export class CloudEntryDoesntExistError extends KeyknoxError {
+export class CloudEntryDoesntExistError extends Error {
   cloudEntryName: string;
 
   constructor(cloudEntryName: string) {
     super(`Cloud entry '${cloudEntryName}' doesn't exist`);
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'CloudEntryDoesntExistError';
     this.cloudEntryName = cloudEntryName;
   }
 }
 
-export class KeyEntryExistsError extends KeyknoxError {
+export class KeyEntryExistsError extends Error {
   keyEntryName: string;
 
   constructor(keyEntryName: string) {
     super(`Key entry '${keyEntryName}' already exists`);
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'KeyEntryExistsError';
     this.keyEntryName = keyEntryName;
   }
 }
 
-export class KeyEntryDoesntExistError extends KeyknoxError {
+export class KeyEntryDoesntExistError extends Error {
   keyEntryName: string;
 
   constructor(keyEntryName: string) {
     super(`Key entry '${keyEntryName}' doesn't exist`);
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'KeyEntryDoesntExistError';
     this.keyEntryName = keyEntryName;
   }
 }
 
-export class GroupSessionMessageInfoAlreadyExistsError extends KeyknoxError {
+export class GroupSessionMessageInfoAlreadyExistsError extends Error {
   constructor() {
     super('GroupSessionMessageInfo already exist');
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'GroupSessionMessageInfoAlreadyExistsError';
   }
 }
 
-export class GroupSessionDoesntExistError extends KeyknoxError {
+export class GroupSessionDoesntExistError extends Error {
   constructor() {
     super("Group session doesn't exist");
+    Object.setPrototypeOf(this, new.target.prototype);
     this.name = 'GroupSessionDoesntExistError';
   }
 }
