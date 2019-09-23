26d80311fae957e9b8d0552e1e1462f749ec830c
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-23 17:41:09 +0300

    v0.4.0-next.0

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export { KeyknoxCrypto } from './KeyknoxCrypto';
 
 export * from './errors';
 
+export { CloudGroupSessionStorage } from './CloudGroupSessionStorage';
 export { CloudKeyStorage } from './CloudKeyStorage';
 export { KeyknoxManager } from './KeyknoxManager';
 export { SyncKeyStorage } from './SyncKeyStorage';
